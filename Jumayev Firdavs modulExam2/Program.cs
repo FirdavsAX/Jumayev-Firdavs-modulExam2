af85fb5f63e15866e81cd255f7d35985b7808561
FirdavsAX <firdavsjumayev320@gmail.com>
2023-09-12 18:53:29 +0500

    Add project files.

---
+++ Jumayev Firdavs modulExam2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jumayev_Firdavs_modulExam2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
